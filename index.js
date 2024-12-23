2d7af2122f79bf443c5f44f4e210c0aac5284713
istiaq-ahsan <ahsanistiaq23@gmail.com>
2024-12-24 02:09:04 +0600

    add so many feature backend

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
 
         const db = client.db('sprint-db')
         const marathonsCollection = db.collection('marathons')
+        const applicantsCollection = db.collection('applicants')
 
         app.post('/add-marathon', async (req, res) => {
             const marathonData = req.body;
@@ -41,6 +42,127 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await marathonsCollection.findOne(query)
+            res.send(result);
+        })
+
+        app.post('/marathon-reg', async (req, res) => {
+            const regData = req.body;
+
+            const query = { email: regData.email, marathonId: regData.marathonId }
+
+            const alreadyExist = await applicantsCollection.findOne(query)
+            if (alreadyExist) {
+                return res.status(400).send('you have already add this marathon');
+            }
+
+            const result = await applicantsCollection.insertOne(regData)
+
+            const filter = { _id: new ObjectId(regData.marathonId) }
+            const update = {
+                $inc: { reg_count: 1 },
+            }
+            const updatedRegCount = await marathonsCollection.updateOne(filter, update)
+
+            res.send(result);
+        })
+
+        app.get('/my-marathons/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { 'creator.email': email }
+
+            const sort = req.query.sort
+
+            let options = {}
+
+            if (sort) options = { sort: { createdAt: sort === 'asc' ? 1 : -1 } }
+
+
+            const result = await marathonsCollection.find(query, options).toArray()
+            res.send(result);
+        })
+
+        app.get('/my-apply/:email', async (req, res) => {
+            const email = req.params.email;
+            const search = req.query.search
+
+            const query = {
+                $and: [
+                    { email: email },
+                    {
+                        title: {
+                            $regex: search,
+                            $options: 'i',
+                        }
+                    }
+                ]
+            }
+
+
+            const result = await applicantsCollection.find(query).toArray()
+            res.send(result);
+
+        })
+
+        app.get('/applyInfo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await applicantsCollection.findOne(query)
+            res.send(result);
+
+        })
+
+        app.put('/update-applyInfo/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const applyData = req.body
+
+            const updated = {
+                $set: applyData
+            }
+
+            const options = { upsert: true }
+
+            const result = await applicantsCollection.updateOne(query, updated, options)
+            res.send(result);
+
+        })
+
+        app.delete('/applyInfo/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await applicantsCollection.deleteOne(query)
+            res.send(result);
+        })
+
+        app.get('/postedMarathon/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await marathonsCollection.findOne(query)
+            res.send(result);
+
+        })
+
+        app.put('/update-marathonInfo/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const marathonData = req.body
+
+            const updated = {
+                $set: marathonData
+            }
+
+            const options = { upsert: true }
+
+            const result = await marathonsCollection.updateOne(query, updated, options)
+            res.send(result);
+
+        })
+
+
+        app.delete('/postedMarathon/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await marathonsCollection.deleteOne(query)
             res.send(result);
         })
 
